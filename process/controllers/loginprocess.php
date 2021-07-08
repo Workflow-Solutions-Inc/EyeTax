075fab375ac45c05bd5d622c7869bc03226c216d
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-08 09:24:49 +0800

    additional updates for useraccounts in eyetax

--- process/controllers/loginprocess.php
+++ process/controllers/loginprocess.php
@@ -5,7 +5,7 @@ session_start();
 
 
 
-	$username = $_POST["username"];
+	/*$username = $_POST["username"];
 	$password = $_POST["password"];
 	if(($username=="admin")&&($password=="admin")){
 		header('location: ../../Main.php');
@@ -22,80 +22,30 @@ session_start();
 			window.location="../../login.php?invalid=1";
 			</script>
 		<?php 
-	}
+	}*/
+include("config/dbconn.php");
+$checker = 0;
+$query = "SELECT * FROM useraccounts where username = '".$_POST["username"]."' and password = '".$_POST["password"]."'";
+$result = $conn->query($query);
+while ($row = $result->fetch_assoc()) 
+{ 
+	$checker = 1;
+	header('location: ../../Main.php');
+}
 
-
-
-?>
-
-
-
-
-
-
-<?php
-/*
-session_start();
-include("dbconn.php");
-
-if($_GET["action"] == "login"){
-$username = $_GET['user'];
-$password = $_GET['pass'];
-$dataareaid = '';
-if($username && $password){
-
-	$sql = "SELECT userid,cast(aes_decrypt(password,'password') as char(50)) as pass,defaultdataareaid from userfile where userid='$username'";
-	$result = $conn->query($sql);
-
-	//if($numrows!=0)
-	if ($result->num_rows > 0){
-		//code to login
-		while ($row = $result->fetch_assoc())
-		{
-			$dbusername = $row['userid'];
-			$dbpassword = $row['pass'];
-			$dataareaid = $row['defaultdataareaid'];
-			//echo $dbusername;
-			//echo $dbpassword;
-			//echo $username;
-			//echo $password;
-			}
-			//check to see if they match!
-			if($username == $dbusername && $password == $dbpassword)
-			{
-				//echo "Login successful! <a href='index.php'>Click</a> here to enter the member page!";
-				$_SESSION['user'] = $dbusername;
-				$_SESSION['defaultdataareaid'] = $dataareaid;
-				header('location: menu.php');
-			}
-			else{
-				header('location: index.php?wrongpassword');
-				//echo "Login successful! ";
-				?>
-					<script type="text/javascript">
-					alert("Invalid Login!")
-					window.location="index.php?invalid"
-					</script>
-				<?php
-			}
-	}
-	else {
-		?>
+if($checker == 0){
+	?>
 			<script type="text/javascript">
-			alert("That user doesn't exist!")
-			window.location="index.php?invalid"
+			alert("That user doesn't exist!");
+			window.location="../../login.php?invalid=1";
 			</script>
-		<?php 
-   }
+	<?php 
 }
 
-else 
-	die("Please enter a username and a password!");
-}
-else if($_GET["action"] == "logout"){
-	session_unset();
-	header('location:index.php');
-}
-*/
+
+
+
 
 ?>
+
+
