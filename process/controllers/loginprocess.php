7f788ca622d90b423564c1dfb6e40306aa9b342d
jonaldarjoncruyz <82009738+jonaldarjoncruyz@users.noreply.github.com>
2021-07-08 13:46:14 +0800

    add a validator if the user logged

--- process/controllers/loginprocess.php
+++ process/controllers/loginprocess.php
@@ -1,28 +1,6 @@
 <?php
 
 session_start();
-//include("dbconn.php");
-
-
-
-	/*$username = $_POST["username"];
-	$password = $_POST["password"];
-	if(($username=="admin")&&($password=="admin")){
-		header('location: ../../Main.php');
-	}
-	else if(($username=="jabogado")&&($password=="adminjabogado")){
-		header('location: ../../Main.php');
-	}
-	else if(($username=="raymond")&&($password=="adminraymond")){
-		header('location: ../../Main.php');
-	}else{
-		?>
-			<script type="text/javascript">
-			alert("That user doesn't exist!");
-			window.location="../../login.php?invalid=1";
-			</script>
-		<?php 
-	}*/
 include("config/dbconn.php");
 $checker = 0;
 $query = "SELECT * FROM useraccounts where username = '".$_POST["username"]."' and password = '".$_POST["password"]."'";
@@ -30,6 +8,7 @@ $result = $conn->query($query);
 while ($row = $result->fetch_assoc()) 
 { 
 	$checker = 1;
+	$_SESSION['userid'] = $row["username"];
 	header('location: ../../Main.php');
 }
 
